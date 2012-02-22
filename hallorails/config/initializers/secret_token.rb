e98daeffe4ba0e46e814e4b308654f644a20d468
SATO-Shinya <satou@agile-corp.com>
2012-02-22 19:13:40 +0900

    'first'

---
+++ hallorails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hallorails::Application.config.secret_token = '0a9478c64663779265b130d0e70e40381f17a9b912ff51d95008ee41e74b3ad55739d61f61321381cadc902de3aca956c1c3a016efebc40882a87bb2bc8f0881'
